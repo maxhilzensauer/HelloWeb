96826e93f7dfa3481530b10ad54f553f6344bc00
maxhilzensauer <max.hilzensauer@htl-salzburg.ac.at>
2019-02-27 17:36:30 +0100

    Insert SceneSwitcher. Everything works fine.

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour {
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void LoadScene2()
+    {
+        SceneManager.LoadScene("scene2");
+    }
+}
